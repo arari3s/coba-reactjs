d8fe1fa438957540b68ff97e14d93eac279f96a6
Arlan Ariandi Trislis <arlanariandi@gmail.com>
2022-07-03 07:09:37 +0700

    UPDATE: Add image sneaker Edit App.css Edit App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Parentbox'>
+      <div className='Foto'>
+        <img src='sneaker.jpg'/>
+      </div>
+      <div className='Deskripsi'>
+        <p className='Cate'>SNEAKER</p>
+        <h1 className='Title'>Nike Free </h1>
+        <p className='Price'>IDR 99.000</p>
+        <p className='Info'>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+      </div>
     </div>
   );
 }
